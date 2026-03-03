e38feb0a784a19844d2ede9c7c03acac0a00803f
coolboy <ateebcoolboyyt@gmail.com>
2026-03-03 20:06:46 +0500

    Fixing(Trying to) Serializers.py

--- media/Serializers.py
+++ media/Serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..accounts.models import Profile
+from accounts.models import Profile
 import os
 from .models import Video, MediaProfile, Comment, CategoryVideo
 
