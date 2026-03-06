4f1f434787f1e2b9414b3aa4b286db9e35fc78f3
coolboy <taburshunu@gmail.com>
2026-03-06 16:28:44 +0500

    Kinda trying to make a superuser

--- burst/settings.py
+++ burst/settings.py
@@ -86,6 +86,7 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://.*\.vercel\.app$",
     r"^https://.*\.railway\.app$",
+
 ]
 # CORS_ORIGIN_ALLOW_ALL = False
 # CORS_ORIGIN_WHITELIST = (
