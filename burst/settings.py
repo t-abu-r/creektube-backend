1efeb7e59d66c99ad9c38ec4a49b521d0ab17262
coolboy <ateebcoolboyyt@gmail.com>
2026-03-06 10:58:45 +0500

    configure cloudinary credentials explicitly

--- burst/settings.py
+++ burst/settings.py
@@ -198,6 +198,14 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')
 }
 
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+    secure=True
+)
+
+
 if DEBUG:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 else:
