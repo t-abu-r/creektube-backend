92e6ad8a7de848b5eca09436db796ad730de4f8a
coolboy <taburshunu@gmail.com>
2026-03-06 12:11:23 +0500

    Fixing APi endpoints for Profile Page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import *
+
 urlpatterns = [
     path("login/", JWTLoginView.as_view(), name="Login"),
     path("register/", JWTRegisterView.as_view(), name="Register"),
     path("logout/", JWTLogoutView.as_view(), name="Logout"),
     path("checkplan/", CheckUserPLan.as_view(), name="check-plan"),
     path("getuser/", CheckUserInfo.as_view(), name="check-user-info"),
-    path("update-profile/", JWTUpdateEmailView.as_view(), name="update-profile"),
-    path("verify-email/", VerifyEmailView.as_view()),
+    path("update-profile/", UpdateProfileView.as_view(), name="update-profile"),
+    path("verify-email/<str:uidb64>/<str:token>/", VerifyEmailView.as_view(), name="verify-email"),
 ]
