ad54964aec805f8a2bddccde28b22c0f7ee78bc7
coolboy <taburshunu@gmail.com>
2026-03-06 13:24:48 +0500

    Revamping some code

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,9 +2,15 @@ from rest_framework import serializers
 from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
-    # Change read_only=True to required=False
     avatar = serializers.ImageField(required=False, allow_null=True)
+    avatar_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
-        fields = ("id", "user", "plan", "avatar")
+        fields = ("id", "user", "plan", "avatar", "avatar_url")
+
+    def get_avatar_url(self, obj):
+        request = self.context.get("request")
+        if obj.avatar:
+            return request.build_absolute_uri(obj.avatar.url)
+        return None
