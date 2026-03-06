49d1aec9da19903c2a0d455e7c29522325d06dd4
coolboy <taburshunu@gmail.com>
2026-03-06 15:27:55 +0500

    PLZZ WORK!

--- accounts/models.py
+++ accounts/models.py
@@ -10,8 +10,6 @@ class PlanChoices(models.TextChoices):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = CloudinaryField('image', blank=True, null=True)
-
-
     bio = models.TextField(blank=True, null=True)
     plan = models.CharField(
         max_length=10,
