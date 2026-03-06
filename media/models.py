e6ff5d65f22b97a1a990e3d6f683ea15aaba6ad3
coolboy <taburshunu@gmail.com>
2026-03-06 14:38:41 +0500

    PLZZ WORK!

--- media/models.py
+++ media/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from ..accounts.models import Profile
 
 # -----------------------------
 # Category model (global)
