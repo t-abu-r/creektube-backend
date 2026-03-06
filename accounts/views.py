1e1f4ef813571eb4763576427a4663b2f5b834dd
coolboy <taburshunu@gmail.com>
2026-03-06 12:09:22 +0500

    Fixing APi endpoints for Profile Page

--- accounts/views.py
+++ accounts/views.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from urllib.parse import unquote
-from ..media.models import MediaProfile
+from media.models import MediaProfile
 
 class VerifyEmailView(APIView):
     permission_classes = [AllowAny]
