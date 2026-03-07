cef726ab808ba54729a27810a787ed9b2bbe6e42
coolboy <taburshunu@gmail.com>
2026-03-07 11:12:33 +0500

    Random

--- burst/settings.py
+++ burst/settings.py
@@ -80,11 +80,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
-CORS_ALLOWED_ORIGINS = os.environ.get(
-    "CORS_ALLOWED_ORIGINS",
-    "https://creektube-frontend.vercel.app"
-).split(",")
-
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "https://railway.com").split(",")
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://.*\.vercel\.app$",
     r"^https://.*\.railway\.app$",
