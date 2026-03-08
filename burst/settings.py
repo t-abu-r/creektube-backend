d166b6e9f7199f1e253b8b039fb42009081ba7ae
coolboy <taburshunu@gmail.com>
2026-03-08 18:25:30 +0500

    * Trying to fix the  Admin Panel...

--- burst/settings.py
+++ burst/settings.py
@@ -36,9 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'cloudinary_storage',  # ← before staticfiles
     'cloudinary',          # ← before staticfiles
-    'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
     'accounts',
