dcf4d5c7dd9d087be1cb3a8b8c2f70744835a5b5
coolboy <taburshunu@gmail.com>
2026-03-08 16:30:20 +0500

    Fixing MediaProfile and Profile Confusion

--- media/views.py
+++ media/views.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 from .permissions import IsModerator
 from .Serializers import VideoSerializer, MediaProfileSerializer
 from accounts.serializers import ProfileSerializer
-from .models import Video, Comment, CategoryVideo, MediaProfile as Profile, MediaProfile
+from .models import Video, Comment, CategoryVideo, MediaProfile
 from django.db.models import Case, When, Q, IntegerField, Count
 from django.views.decorators.csrf import csrf_exempt
 import os
