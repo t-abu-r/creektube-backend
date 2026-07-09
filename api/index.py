748007685e1ad2bc74a0629009fc6c7653e48f10
t-abu-r <taburshunu@gmail.com>
2026-07-09 15:51:14 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,10 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, BASE_DIR)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "burst.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "burst.settings.prod"
+)
 
 from django.core.wsgi import get_wsgi_application
 
