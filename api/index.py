01825163c849b22313e2378634b6362e00afc7f3
t-abu-r <taburshunu@gmail.com>
2026-07-09 15:49:29 +0500

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "burst.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+app = get_wsgi_application()
