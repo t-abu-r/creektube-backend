91ea1601051d7db874c45866d89e3bea6659642a
coolboy <taburshunu@gmail.com>
2026-07-10 12:36:34 +0500

    Fix YA<eeeeeeeML File Syntrax

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from media.models import MediaProfile
 
-# Create your tests here.
+
+class ProfileCreationTest(TestCase):
+    def test_media_profile_created_via_view(self):
+        user = User.objects.create_user(username="testuser", password="testpass123")
+        MediaProfile.objects.get_or_create(user=user)
+        self.assertTrue(MediaProfile.objects.filter(user=user).exists())
