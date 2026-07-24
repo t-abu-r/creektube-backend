c11568c1cd9672151bc85215a5fab3f1d7084162
coolboy <taburshunu@gmail.com>
2026-07-24 19:23:41 +0500

    Add SMTP fixes

--- burst/settings/base.py
+++ burst/settings/base.py
@@ -122,7 +122,7 @@ REST_FRAMEWORK = {
 }
 
 # Email settings
-EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp-relay.brevo.com")
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True").lower() == "true"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
