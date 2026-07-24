39391be358d00e07fdf9629f247307ab602eac1e
coolboy <taburshunu@gmail.com>
2026-07-24 19:07:10 +0500

    Add SMTP fixes

--- burst/settings/base.py
+++ burst/settings/base.py
@@ -127,7 +127,7 @@ EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True").lower() == "true"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", f"CreekTube <{EMAIL_HOST_USER}>" if EMAIL_HOST_USER else None)
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", f"OutReach <outreach@germanypathway.com>" if EMAIL_HOST_USER else None)
 PASSWORD_RESET_TIMEOUT = int(os.environ.get("PASSWORD_RESET_TIMEOUT", 60 * 60 * 24))
 
 # Cloudinary settings
